565f651172adfaae0077c201912257634891efdb
MellowZtmy <tappynanakoro@gmail.com>
2025-07-18 03:45:44 +0900

    選択画面仮実装

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ $(document).ready(async function () {
 });
 
 // 画面タグ作成
-function createDisplay(mode, alubumName = '', puzzleIndex = -1) {
+function createDisplay(mode, alubumName = '', artifactIndex = 0) {
   // 少し待ってから処理を開始（スピナー表示のため、DOM描画を反映させるため）
   setTimeout(() => {
     try {
@@ -123,160 +123,56 @@ function createDisplay(mode, alubumName = '', puzzleIndex = -1) {
       } else if (mode === display.SELECT) {
         //////////////////////////////////////////
         // SELECT画面
+        //   引数：alubumName
         //////////////////////////////////////////
-        var quiz = quizzes[currentQuizIndex];
-        // 『』で囲む対象の区別
-        let isgameModeLyricToSong =
-          getLocal('gameMode') === gameMode.LYRIC_TO_SONG.VALUE;
-        tag += ' ';
-        tag += ' <!-- 問題番号 -->';
-        tag +=
-          ' <h2 class="h2-display">Question. ' +
-          (currentQuizIndex + 1) +
-          ' / ' +
-          quizzes.length +
-          '</h2>';
-        tag += ' ';
-        tag += ' <!-- 問題文 -->';
-        tag += ` <p class="font-one-point-five reveal">
-        ${
-          (isgameModeLyricToSong ? '『' : '') +
-          quiz.question +
-          (isgameModeLyricToSong ? '』' : '')
-        }</p>`;
-        tag += ' ';
-        tag += ' <!-- 選択肢のラジオボタン + ラベル -->';
-        quiz.choices.forEach((choice, index) => {
-          tag += '   <label';
-          tag += '     class="choice-radio-label"';
-          tag += '   >';
-          tag += '     <input';
-          tag += '       type="radio"';
-          tag += '       id="choice' + index + '"';
-          tag += '       value="' + index + '"';
-          tag += '       name="choices"';
-          tag += '       onchange="onSelect(' + index + ')"';
-          tag += '     >';
-          tag += '     <span class="left-text">';
+
+        // アーティファクト情報取得
+        let dispArtifacts = artifacts.filter(
+          (row) => row[appsettings.albumNameCol] === alubumName
+        );
+
+        // タグ生成
+        tag += ' <h2 class="h2-display">Artifacts Select</h2>';
+        tag += '<div class="album-list">';
+        dispArtifacts.forEach(function (dispArtifact, index) {
           tag +=
-            '     ' +
-            (isgameModeLyricToSong ? '' : '『') +
-            choice +
-            (isgameModeLyricToSong ? '' : '』');
-          tag += '     </span>';
-          tag +=
-            '     <span id="marubatu' +
+            '<div class="album-item" onclick="createDisplay(display.PUZZLE,\'' +
+            alubumName +
+            "'," +
             index +
-            '" class="right-text bold-text font-one-point-five">';
-          tag += '     ';
-          tag += '     </span>';
-          tag += '   </label>';
-          tag += ' ';
+            ')">';
+          tag +=
+            ' <img src="' +
+            appsettings.albumImagePath +
+            +(index + 1) +
+            '_' +
+            alubumName +
+            '.jpg" id="' +
+            alubumName +
+            '" name="album" alt="' +
+            alubumName +
+            '" class="album">';
+          tag +=
+            '<div class="album-title">' +
+            dispArtifact[appsettings.artifactsNameCol] +
+            '</div>';
+          tag += ' </div>';
         });
+        tag += ' </div>'; // album-list
 
-        tag += ' ';
-        tag += ' <!-- 次へ / 終了 ボタン -->';
-        tag += quizzes[currentQuizIndex + 1]
-          ? '   <button id="next" onclick="loadQuiz(false)" class="btn btn--main btn--radius btn--cubic visibility-hidden">NEXT→</button>'
-          : '   <button id="result" onclick="showResult()" class="btn btn--main btn--radius btn--cubic visibility-hidden">RESULT</button>';
-        // MV表示
-        tag += '    <!--MV Youtube--> ';
-        tag += '    <div class="margin-top-20" id="mv" hidden> ';
+        tag += ' <!-- 戻る ボタン -->';
         tag +=
-          '      <div style="position: relative; width: 100%; padding-bottom: 56.25%"> ';
-        tag += '        <div ';
-        tag += '          style=" ';
-        tag += '            position: absolute; ';
-        tag += '            top: 0px; ';
-        tag += '            left: 0px; ';
-        tag += '            width: 100%; ';
-        tag += '            height: 100%; ';
-        tag += '          " ';
-        tag += '        > ';
-        tag += '          <iframe ';
-        tag +=
-          '            src="https://www.youtube.com/embed/' +
-          quiz.mvId +
-          '?loop=1&playlist=' +
-          quiz.mvId +
-          '" ';
-        tag += '            frameborder="0" ';
-        tag += '            width="100%" ';
-        tag += '            height="100%"  style="border-radius: 15px;"';
-        tag += '            allowfullscreen="" ';
-        tag += '          ></iframe> ';
-        tag += '        </div> ';
-        tag += '      </div> ';
-        tag += `    <p class="right-text-no-ellipsis"> ずっと真夜中でいいのに。<br>`;
-        tag += `    『${quiz.song}』<br>`;
-        tag += `    （作詞 ： ${quiz.lyricist}）</p>`;
-        tag += '    </div> ';
+          '   <button id="back" onclick="createDisplay(display.TOP)" class="btn btn--main btn--radius btn--cubic">←BACK</button>';
+      } else if (mode === display.PUZZLE) {
+        //////////////////////////////////////////
+        // PUZZLE画面
+        //////////////////////////////////////////
+        tag += ' <h2 class="h2-display">Puzzle</h2>';
       } else if (mode === display.RESULT) {
-        // 問題数取得
-        var quizzesLength = quizzes.length;
-        // 正解数取得
-        var correctCount = selectedList.filter(
-          (value, index) => value === quizzes[index].correctAnswer
-        ).length;
+        //////////////////////////////////////////
         // RESULT画面
-        // 正解数表示
-        tag +=
-          ' <h2 class="center-text' +
-          (correctCount === quizzesLength ? ' text-correct' : '') +
-          '">' +
-          correctCount +
-          ' / ' +
-          quizzesLength +
-          '</h2>';
-        tag +=
-          correctCount === quizzesLength
-            ? '<h2 class="center-text text-correct">PERFECT!!</h2>'
-            : '';
-        // Result表示
+        //////////////////////////////////////////
         tag += ' <h2 class="h2-display">Result</h2>';
-        quizzes.forEach((quiz, index) => {
-          tag +=
-            ' <div class="font-one-point-two">Q' +
-            (index + 1) +
-            '. ' +
-            quiz.question +
-            '</div>';
-          tag +=
-            ' <div class="font-one-point-two right-text ' +
-            (selectedList[index] === quiz.correctAnswer ? 'text-correct' : '') +
-            '">' +
-            quiz.choices[quiz.correctAnswer] +
-            '</div>';
-          tag += index === quizzes.length - 1 ? '' : '<br>';
-        });
-        // アルバム表示
-        tag +=
-          selectedAlbums.length > 0
-            ? ' <h2 class="h2-display">Albums</h2>'
-            : '';
-        albums.forEach(function (album, index) {
-          if (selectedAlbums.includes(album)) {
-            tag +=
-              ' <img src="' +
-              appsettings.albumImagePath +
-              +(index + 1) +
-              '_' +
-              album +
-              '.jpg" id="' +
-              album +
-              '" name="album" alt="' +
-              album +
-              '" class="album">';
-          }
-        });
-
-        tag +=
-          ' <button id="retry" onclick="createDisplay(display.TOP)" class="btn btn--main btn--radius btn--cubic">RETRY</button>';
-
-        // ハイスコア設定(「??」は「<」より優先度が低いのでカッコをつける
-        if ((Number(getLocal('ztmyLyricQuizHighScore')) ?? 0) < correctCount) {
-          setLocal('ztmyLyricQuizHighScore', correctCount);
-        }
       }
 
       // タグ流し込み
